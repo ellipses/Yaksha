a369556d576881c900de020a4a4e18a11f16b5f5
ellipses <prasadt@tcd.ie>
2016-05-27 01:48:48 +0100

    adding frinkiac functionality

--- discord-bot.py
+++ discord-bot.py
@@ -2,45 +2,69 @@
 import discord
 import asyncio
 import re
-from utils import Streams
+import yaml
+import utils
 
 global client
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('--------')
-	servers = client.servers
-	for server in servers:
-		print(server)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------')
+    servers = client.servers
+    for server in servers:
+        print('Joined server %s' % server)
 
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('!add'):
-		channel = re.search(r'^!add (\w+)', message.content).group(1)
-		client.streams.add_channel(channel)
-		await client.send_message(message.channel, 'Added channel %s' % channel)
-	elif message.content.startswith('whens'):
-		await client.send_message(message.channel, client.streams.display_stream_list())
-	elif message.content.startswith('!sleep'):
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
+    '''
+    List of commands that are perfomed by the bot.
+
+    !add channel: adds the channel to the channel.txt text file.
+
+    !whens: Lists the currently online streams, title and current viewers.
+            Based on the list of channels in channel.txt
+
+    !simpsons caption: Returns a gif from Frinkiac containing the caption.
+    '''
+    if message.content.startswith('!add'):
+        channel = re.search(r'^!add (\w+)', message.content).group(1)
+        client.streams.add_channel(channel)
+        await client.send_message(message.channel,
+                                 'Added channel https://twitch.tv/%s' % channel)
+
+    elif message.content.startswith('whens'):
+        await client.send_message(message.channel,
+                                  client.streams.display_stream_list())
+
+    elif message.content.startswith('!simpsonscaption'):
+        caption = message.content.strip('!simpsonscaption')
+        await client.send_message(message.channel,
+                            client.frinkiac.get_gif(caption, text=True))
+
+    elif message.content.startswith('!simpsons'):
+        caption = message.content.strip('!simpsons')
+        await client.send_message(message.channel,
+                            client.frinkiac.get_gif(caption))
+
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
 
 
 def main():
-	client.streams = Streams()
-	# NEED TO MAKE THIS CONFIG
-	email = ''
-	password = ''
-	client.run(email, password)
-
+    client.streams = utils.Streams()
+    client.frinkiac = utils.Frinkiac()
+    config_path = 'bots.yaml'
+    config = yaml.load(open(config_path).read())
+    email = config['discord']['email']
+    password = config['discord']['password']
+    client.run(email, password)
 
 if __name__ == '__main__' :
-	main()
-
-
-
+    main()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import requests
+import base64
 import json
 
 
@@ -10,7 +11,6 @@ class Streams():
         self.api_prefix = 'https://api.twitch.tv/kraken/streams/?channel='
 
     def add_channel(self, channel_name):
-        # Todo: 
         with open(self.file, 'a') as file:
             file.write(' %s ' % channel_name)
 
@@ -34,7 +34,7 @@ class Streams():
             message += channel_format % (url, title, viewers)
 
         return message
-        
+
     def display_stream_list(self):
         channels = self.get_channels()
         channel_list = (',').join(channels.split(' '))
@@ -46,3 +46,84 @@ class Streams():
         else:
             print ('Failed getting steam data with error %s' % payload.text)
             return False
+
+
+class Frinkiac():
+
+    def __init__(self):
+        # gif/episode/start_timestamp/end_timestamp.gif?b64lines=caption_in_base64
+        self.gif_url = 'https://frinkiac.com/gif/%s/%s/%s.gif'
+        self.caption_url = 'https://frinkiac.com/gif/%s/%s/%s.gif?b64lines=%s'
+        self.api_url = 'https://frinkiac.com/api/search?q=%s'
+        self.interval = 350
+        self.max_count = 31
+        self.max_timespan = 6200  # 31 * 200
+
+    def get_max_sequence(self, timestamps):
+        '''
+        '''
+        # Find the longest sequence
+        total_seqs = []
+        current_seq = []
+        prev_value = timestamps[0]
+        for value in timestamps[1:]:
+            if value - prev_value > self.interval:
+                total_seqs.append(current_seq)
+                current_seq = []
+
+            current_seq.append(value)
+            prev_value = value
+        total_seqs.append(current_seq)
+
+        seq_lens = [len(seq) for seq in total_seqs]
+        index = seq_lens.index(max(seq_lens))
+        # Only return up to max count to stay under timelimit
+        return total_seqs[index][:self.max_count]
+
+    def get_timestamps(self, screencaps):
+        '''
+        '''
+        episodes = {}
+        timestamps = {}
+
+        for screencap in screencaps:
+            episode = screencap['Episode']
+            timestamp = screencap['Timestamp']
+
+            if episode in episodes:
+                episodes[episode] += 1
+                timestamps[episode].append(timestamp)
+            else:
+                episodes[episode] = 1
+                timestamps[episode] = [timestamp]
+
+        # If the most common episode
+        # Get the first one if more than 1 exist.
+        max_count = max(episodes.values())
+        for name, count in episodes.items():
+            if count == max_count:
+                episode = name
+                break
+
+        max_seq = self.get_max_sequence(sorted(timestamps[episode]))
+        return episode, max_seq
+
+    def get_gif(self, caption, text=False):
+        # Hit the api endpoint to get a list of screencaps
+        # that the relevant to the caption.
+        response = requests.get(self.api_url % caption)
+        screen_caps = json.loads(response.text)
+
+        if len(screen_caps) == 0:
+            message = 'Try saying a line that actually exists'
+            return message
+
+        # Find the most common episode and longest sequence of
+        # timestamps.
+        episode, timestamps = self.get_timestamps(screen_caps)
+
+        if text:
+            encoded = str(base64.b64encode(str.encode(caption)), 'utf-8')
+            return self.caption_url % (episode, timestamps[0], timestamps[-1], encoded)
+        else:
+            return self.gif_url % (episode, timestamps[0], timestamps[-1])
