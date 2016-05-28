a5a6c4b48fb1294d9bc001b9db5533ea90cf2bad
ellipses <prasadt@tcd.ie>
2016-05-28 01:28:48 +0100

    fixing frinkiac and some arbitary stuff

--- discord-bot.py
+++ discord-bot.py
@@ -43,14 +43,28 @@ async def on_message(message):
                                   client.streams.display_stream_list())
 
     elif message.content.startswith('!simpsonscaption'):
-        caption = message.content.strip('!simpsonscaption')
+        caption = message.content[len('!simpsonscaption') + 1:]
         await client.send_message(message.channel,
                             client.frinkiac.get_gif(caption, text=True))
 
+    elif message.content.startswith('!simpsonsdebug'):
+        caption = message.content[len('!simpsonsdebug') + 1:]
+        print('received msg')
+        print(message.content)
+        print('caption')
+        print(caption)
+        await client.send_message(message.channel,
+                            client.frinkiac.get_gif(caption, debug=True))
+
     elif message.content.startswith('!simpsons'):
-        caption = message.content.strip('!simpsons')
+        caption = message.content[len('!simpsons') + 1:]
         await client.send_message(message.channel,
                             client.frinkiac.get_gif(caption))
+
+    elif message.content.startswith('!shuffle'):
+        sentence = message.content[len('!shuffle') + 1:]
+        await client.send_message(message.channel,
+                            client.arbitary.shuffle(sentence, message.author.mention))
 
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
@@ -58,10 +72,12 @@ async def on_message(message):
 
 
 def main():
-    client.streams = utils.Streams()
-    client.frinkiac = utils.Frinkiac()
     config_path = 'bots.yaml'
     config = yaml.load(open(config_path).read())
+    client.streams = utils.Streams()
+    client.frinkiac = utils.Frinkiac(config['frinkiac'])
+    client.arbitary = utils.Arbitary()
+
     email = config['discord']['email']
     password = config['discord']['password']
     client.run(email, password)
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import requests
+import random
 import base64
 import json
-
+import re
 
 class Streams():
 
@@ -50,23 +51,26 @@ class Streams():
 
 class Frinkiac():
 
-    def __init__(self):
+    def __init__(self, config):
         # gif/episode/start_timestamp/end_timestamp.gif?b64lines=caption_in_base64
         self.gif_url = 'https://frinkiac.com/gif/%s/%s/%s.gif'
         self.caption_url = 'https://frinkiac.com/gif/%s/%s/%s.gif?b64lines=%s'
         self.api_url = 'https://frinkiac.com/api/search?q=%s'
-        self.interval = 350
+        self.interval = 300
         self.max_count = 31
-        self.max_timespan = 6200  # 31 * 200
+        self.max_timespan = 6200 
+        self.char_limit = 20
 
-    def get_max_sequence(self, timestamps):
+    def get_max_sequence(self, timestamps, debug=False):
         '''
+        Helper function that returns the longest consequtive
+        sequence of timestamps seperated by at most self.interval
+        seconds.
         '''
-        # Find the longest sequence
         total_seqs = []
         current_seq = []
         prev_value = timestamps[0]
-        for value in timestamps[1:]:
+        for value in timestamps:
             if value - prev_value > self.interval:
                 total_seqs.append(current_seq)
                 current_seq = []
@@ -77,11 +81,23 @@ class Frinkiac():
 
         seq_lens = [len(seq) for seq in total_seqs]
         index = seq_lens.index(max(seq_lens))
-        # Only return up to max count to stay under timelimit
+
+        if debug:
+            print('Total')
+            print(timestamps)
+            print('Selected')
+            print(total_seqs)
+            print('length')
+            print(len(total_seqs))
+
+        # Only return up to max count to stay under timelimit.
         return total_seqs[index][:self.max_count]
 
-    def get_timestamps(self, screencaps):
+    def get_timestamps(self, screencaps, debug=False):
         '''
+        Helper function that iterates through the list returned
+        by the api endpoint to find the episode and the longest
+        sequence of timestamps.
         '''
         episodes = {}
         timestamps = {}
@@ -105,25 +121,94 @@ class Frinkiac():
                 episode = name
                 break
 
-        max_seq = self.get_max_sequence(sorted(timestamps[episode]))
+        if debug:
+            print('epside count')
+            print(episodes)
+            print(episode)
+            print('screencaps')
+            print(screencaps)
+            print('timestamps')
+            print(timestamps)
+
+        max_seq = self.get_max_sequence(sorted(timestamps[episode]), debug=debug)
         return episode, max_seq
 
-    def get_gif(self, caption, text=False):
+    def format_message(self, message):
+        '''
+        Formats the message by adding line breaks to prevent it
+        from overflowing the gifs boundry. Line breaks are added
+        at the end of a word to prevent it from being split.
+        '''
+        # Iterate thr
+        char_buff = 0
+        formated_msg = ''
+        for word in message:
+            char_buff += len(word)
+            formated_msg += ' %s' % word
+            if char_buff >= 20:
+                char_buff = 0
+                formated_msg += '/n'
+
+        return formated_msg
+
+    def get_gif(self, caption, text=False, debug=False):
+        '''
+        Main function that called to retreive a gif url.
+
+        Args:
+            caption: The message thats used hit the api endpoint.
+            text: Controls wether the generated gif will contain
+                  the caption.
+        '''
         # Hit the api endpoint to get a list of screencaps
-        # that the relevant to the caption.
+        # that are relevant to the caption.
         response = requests.get(self.api_url % caption)
         screen_caps = json.loads(response.text)
 
-        if len(screen_caps) == 0:
-            message = 'Try saying a line that actually exists'
+        if debug:
+            print('search url')
+            print(self.api_url % caption)
+            print('response')
+            print(screen_caps)
+
+        if len(screen_caps) == 1:
+            message = 'Try saying a line that actually exists.'
             return message
 
         # Find the most common episode and longest sequence of
         # timestamps.
-        episode, timestamps = self.get_timestamps(screen_caps)
+        episode, timestamps = self.get_timestamps(screen_caps, debug=debug)
+
+        seq_length = len(timestamps)
+        if seq_length <= 1:
+            return 'Try fixing your quote'
 
         if text:
+            # Perform base 64 encoding if a captioned version of the gif was
+            # requested.
+            # caption = self.format_message(caption)
             encoded = str(base64.b64encode(str.encode(caption)), 'utf-8')
             return self.caption_url % (episode, timestamps[0], timestamps[-1], encoded)
         else:
             return self.gif_url % (episode, timestamps[0], timestamps[-1])
+
+class Boards():
+
+    def __init__(self):
+        pass
+
+class Arbitary():
+
+    def __init__(self):
+        pass
+
+    def shuffle(self, sentence, author):
+        sentence = re.sub(r'\s\s+', ' ', sentence)
+        word_list = sentence.split(' ')
+
+        if word_list == ['']:
+            return "Look %s You can't expect me to shuffle nothing." % author
+        elif len(word_list) == 1:
+            return 'Dont waste my time %s. Shuffling 1 word is pointless' % author
+        else:
+            return random.choice(word_list)
