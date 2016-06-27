cd88fc7732b95add1ca850efc401ea6167c93c68
ellipses <prasadt@tcd.ie>
2016-06-27 03:31:59 +0100

    fixing up imports and getting irc working

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from commands import ifgc, actions, voting
 import pydle
 import yaml
 import time
@@ -80,19 +81,62 @@ class Bot():
 '''
 
 class MyClient(pydle.Client):
-    """ This is a simple bot that will greet people as they join the channel. """
 
     def __init__(self, channels, *args, **kwawgs):
+        '''
+        '''
         self.channels_to_join = channels
+        config_path = 'bots.yaml'
+        self.config = yaml.load(open(config_path).read())
+
+        frinkiac = actions.Frinkiac()
+        arbitary = actions.Arbitary()
+        gifs = actions.Gifs()
+        tourney = actions.Tourney()
+        boards = ifgc.Boards()
+        frames = ifgc.Frames(self.config['frame_data'])
+        commands = actions.AddCommands(self.config['add_commands']['irc'])
+
+        self.simpsons_gif = frinkiac.get_gif
+        self.captioned_gif = frinkiac.get_captioned_gif
+        self.shuffle = arbitary.shuffle
+        self.casuals = boards.get_thread_posters
+        self.tourney = tourney.get_tourneys
+        self.giffy_gif = gifs.get_gif
+        self.get_frames = frames.get_frames
+        self.add_command = commands.add_command
+        self.get_command = commands.get_command  
+
+        self.commands = self.config['common-actions']
+
         super().__init__(*args, **kwawgs)
 
+    def add_commands(self):
+        '''
+        '''
+        pass
+
     def on_connect(self):
+        '''
+        '''
         for channel in self.channels_to_join:
             self.join(channel)
             print('Connected to %s' % channel) 
 
     def on_message(self, channel, user, msg):
-        pass
+        '''
+        '''
+        if channel == self.nickname:
+            self.message(channel, ("Sneaky communication isn't nice,"
+                                   " play with the group"))
+        else:
+            for command in self.commands.keys():
+                if msg.lower().startswith(command.lower()):
+                    msg = msg[len(command):].strip()
+                    response = getattr(self, self.commands[command])(msg,
+                                                                     user)
+                    self.message(channel, response)
+                    break
 
 
 def main():
