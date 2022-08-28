8886858d2efbcfc45056498eb5a981f8874a384a
Name <email>
2022-08-28 19:42:33 +0100

    Massive update to support slash commands and cleanup

--- src/commands/utilities.py
+++ src/commands/utilities.py
@@ -4,15 +4,15 @@ from functools import wraps
 import functools
 import requests
 import aiohttp
-import asyncio
+import logging
 import time
 
 
 def rate_limit(time_gap):
-    '''
+    """
     Decorator that limits how often a user can use
     a function.
-    '''
+    """
     _time = {}
 
     def rate_decorator(func):
@@ -33,92 +33,106 @@ def rate_limit(time_gap):
                 else:
                     delta = timedelta(seconds=time_gap - time_diff)
                     time_format = datetime(1, 1, 1) + delta
-                    return ("Nice try %s, but I've already done this for you. "
-                            'You can ask me again in %s days, %s hours, %s'
-                            ' minutes and %s seconds.') % (user,
-                                                           time_format.day - 1,
-                                                           time_format.hour,
-                                                           time_format.minute,
-                                                           time_format.second
-                                                           )
+                    return (
+                        "Nice try %s, but I've already done this for you. "
+                        "You can ask me again in %s days, %s hours, %s"
+                        " minutes and %s seconds."
+                    ) % (
+                        user,
+                        time_format.day - 1,
+                        time_format.hour,
+                        time_format.minute,
+                        time_format.second,
+                    )
 
             else:
                 _time[hash_key] = time.time()
                 return await func(*args, **kwargs)
 
         return func_wrapper
+
     return rate_decorator
 
 
 def memoize(cache_time):
-    '''
+    """
     Decorator that memoizes the result of the function call
     for the specified time period.
-    '''
+    """
     _cache = {}
 
     def memoize_decorator(func):
         @wraps(func)
         async def func_wrapper(*args, **kwargs):
-            '''
+            """
             Returns the cached value if it exists and otherwise
             it evalues the function and stores it in the cache.
             no_cache=True can be passed to the function to prevent
             retrieving from the cache.
-            '''
-            no_cache = kwargs.get('no_cache', False)
+            """
+            no_cache = kwargs.get("no_cache", False)
             if func in _cache and not no_cache:
                 stored_time = _cache[func][1]
 
                 if time.time() - stored_time > cache_time:
+                    logging.info("cache expired for %s so refilling", func)
                     returned_result = await func(*args, **kwargs)
                     _cache[func] = (returned_result, time.time())
 
                 return _cache[func][0]
 
             else:
+                logging.info("no cache for %s, so cache busting", func)
                 returned_result = await func(*args, **kwargs)
                 _cache[func] = (returned_result, time.time())
                 return returned_result
 
         return func_wrapper
+
     return memoize_decorator
 
 
 _callbacks = {}
+
+
 def register(command):
-    '''
+    """
     _Registers_ each function with by storing the command its name
     into a dict.
-    '''
+    """
+
     def decorator(func):
-        print('Registering %s with command %s' % (func.__name__, command))
+        print("Registering %s with command %s" % (func.__name__, command))
         _callbacks[command] = (func.__qualname__, func.__module__)
         return func
+
     return decorator
 
 
 def get_callbacks():
-    '''
+    """
     Simple getter that returns the dictionary containing
     the registered functions. Might be better to make
     registration into a class instead.
-    '''
+    """
     return _callbacks
 
 
-async def get_request(url, json=False):
-    '''
+async def get_request(url, response_headers):
+    """
     Wrapper to handle get requests asyncronously.
     Returns the text if the status is 200, False
     otherwise.
-    '''
+    """
+    headers = {}
+    if response_headers:
+        headers = {
+            "etag": response_headers["etag"],
+            "If-Modified-Since": response_headers["Last-Modified"],
+        }
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
+        async with session.get(url, headers=headers) as resp:
+            logging.info("Request response is %s", resp.status)
             if resp.status == 200:
-                if json:
-                    return await resp.json()
-                else:
-                    return await resp.text()
-            else:
-                return False
+                return await resp.json(), resp.headers
+            return False, resp.headers
--- src/irc-bot.py
+++ src/irc-bot.py
@@ -13,49 +13,49 @@ import re
 
 @irc3.plugin
 class MyClient(object):
-
     def __init__(self, bot):
         self.bot = bot
         self.nick = self.bot.get_nick()
-        config_path = os.path.join(os.path.dirname(__file__),
-                                   '../conf/bots.yaml')
+        config_path = os.path.join(os.path.dirname(__file__), "../conf/bots.yaml")
         self.config = yaml.load(open(config_path).read())
-        self.channels = self.config['irc']['channels']
-        self.commands = self.config.get('common_actions', {})
-        self.commands.update(self.config.get('irc_actions', {}))
-        self.commands.update(self.config.get('admin_actions', {}))
-        self.url_enabled_channels = self.config.get('youtube', {}).get(
-            'url_enabled_channels', []
+        self.channels = self.config["irc"]["channels"]
+        self.commands = self.config.get("common_actions", {})
+        self.commands.update(self.config.get("irc_actions", {}))
+        self.commands.update(self.config.get("admin_actions", {}))
+        self.url_enabled_channels = self.config.get("youtube", {}).get(
+            "url_enabled_channels", []
         )
         self.interface = interface.Interface(self.config, self.commands)
-        self.youtube_regex = '(?:https?://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?)?v=([^\s]+)'
+        self.youtube_regex = (
+            "(?:https?://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?)?v=([^\s]+)"
+        )
         self.build_youtube_service()
 
     def build_youtube_service(self):
         try:
             self.service = build(
-                'youtube', 'v3', 
-                developerKey=self.config['youtube']['api_key'])
+                "youtube", "v3", developerKey=self.config["youtube"]["api_key"]
+            )
         except KeyError:
-            logging.info('Starting without youtube service')
+            logging.info("Starting without youtube service")
             self.service = None
-    
+
     @irc3.event(irc3.rfc.CONNECTED)
     def connected(self, **kw):
         for channel in self.channels:
             self.bot.join(channel)
-            print('Joined %s' % channel)
+            print("Joined %s" % channel)
 
     async def send_message(self, channel, message):
-        '''
+        """
         Async send method to maintain compatibility
         with discord format.
-        '''
+        """
         await self.bot.privmsg(channel, message)
 
     @irc3.event(irc3.rfc.PRIVMSG)
     async def on_privmsg(self, mask, data, target, **kw):
-        '''
+        """
         irc3 method thats called everytime there is a message.
         Doesn't do anything except pass it on to method that
         actually handles it.
@@ -63,14 +63,14 @@ class MyClient(object):
             mask: user
             data: message
             target: channel
-        '''
+        """
         await self.handle_message(mask, data, target)
 
     async def check_url_parsing(self, msg, channel):
         if channel in self.url_enabled_channels:
             match_obj = re.search(self.youtube_regex, msg)
             if match_obj:
-               await self.return_youtube_title(channel, match_obj.group(1))
+                await self.return_youtube_title(channel, match_obj.group(1))
 
     async def return_youtube_title(self, channel, video_id):
         if self.service:
@@ -78,42 +78,43 @@ class MyClient(object):
                 None, functools.partial(self.perform_youtube_request, video_id)
             )
             if result:
-                msg = 'YouTube: %s (%s)' % result
+                msg = "YouTube: %s (%s)" % result
                 await self.send_message(channel, msg)
 
     def perform_youtube_request(self, video_id):
         try:
-            result = self.service.videos().list(
-                part='snippet, contentDetails',id=video_id
-            ).execute()['items'][0]
-            title = result['snippet']['title']
-            duration = isodate.parse_duration(result['contentDetails']['duration'])
-            mins = int(duration.seconds/60)
+            result = (
+                self.service.videos()
+                .list(part="snippet, contentDetails", id=video_id)
+                .execute()["items"][0]
+            )
+            title = result["snippet"]["title"]
+            duration = isodate.parse_duration(result["contentDetails"]["duration"])
+            mins = int(duration.seconds / 60)
             seconds = int(duration.seconds - mins * 60)
-            return(title, '%s:%s' % (mins, seconds))
+            return (title, "%s:%s" % (mins, seconds))
         # Pokemon exception catching because we dont care as much about parsing
         # youtube titles.
         except Exception:
-            logging.exception('failed to obtain youtube title')
+            logging.exception("failed to obtain youtube title")
             return None
 
     async def handle_message(self, user, msg, channel):
-        '''
+        """
         Main method that determines how a received message is handled.
-        '''
+        """
         # Don't bother doing anything with msgs sent by us.
-        if self.nick != user[:len(self.nick)]:
+        if self.nick != user[: len(self.nick)]:
             # Only bother with non private msgs.
             if channel != self.nick:
                 await self.check_url_parsing(msg, channel)
                 for command in self.commands.keys():
                     if msg.lower().startswith(command.lower()):
-                        msg = msg[len(command):].strip()
+                        msg = msg[len(command) :].strip()
                         command = command.lower()
-                        response = await self.interface.call_command(command,
-                                                                     msg, user,
-                                                                     channel,
-                                                                     self)
+                        response = await self.interface.call_command(
+                            command, msg, user, channel, self
+                        )
 
                         if response:
                             if isinstance(response, tuple):
